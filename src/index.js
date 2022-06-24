9b5a5c9e42fab633af07e860486ce4a65d79ee68
giapbnv <nguyen.van.giap-b@sun-asterisk.com>
2022-06-24 14:58:44 +0700

    example using route

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import express from "express"
-import { engine } from "express-handlebars"
-import * as path from "path"
-import { fileURLToPath } from "url"
-import morgan from "morgan"
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const express = require("express")
+const handlebars = require("express-handlebars")
+const path = require("path")
+const morgan = require("morgan")
+const birds = require("./birds")
 const app = express()
 const port = 3000
 app.use(morgan("combined"))
@@ -12,7 +10,7 @@ app.use(morgan("combined"))
 app.use("/statics", express.static(path.resolve(__dirname, "public")))
 //template engine
 
-app.engine(".hbs", engine({extname: ".hbs"}))
+app.engine(".hbs", handlebars.engine({extname: ".hbs"}))
 app.set("view engine", ".hbs")
 app.set("views", path.resolve(__dirname, "./resources/views"))
 // app.set("views", path.join(__dirname, "./resources/views"))
@@ -20,6 +18,19 @@ app.set("views", path.resolve(__dirname, "./resources/views"))
 app.get("/", (req, res) => {
   res.render("home")
 })
+app.route("/book")
+  .get((req, res) => {
+    res.send("Get a random book")
+  })
+  .post((req, res) => {
+    res.send("Add a book")
+  })
+  .put((req, res) => {
+    res.send("Update the book${}")
+  })
+
+// import bird
+app.use("/birds", birds)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}, view"s dir: ${path.resolve(__dirname, "./resources/views")}`)
